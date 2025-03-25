0f321eda238bd5012e8ebbc58777e18c4866f687
Krittin Wankundee <krittin.wankundee@gmail.com>
2025-03-25 12:04:29 +0700

    Edit reservation model

--- models/Reservation.js
+++ models/Reservation.js
@@ -10,6 +10,10 @@ const ReservationSchema = new mongoose.Schema({
         ref: 'User',
         required: true
     },
+    userName: {
+        type: String,
+        required: true
+    },
     restaurant: {
         type: mongoose.Schema.ObjectId,
         ref: 'Restaurant',
