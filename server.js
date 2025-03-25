094b816f8f13f5ea03a18e4721f7e661915a24d8
Krittin Wankundee <krittin.wankundee@gmail.com>
2025-03-26 00:14:22 +0700

    Add HOST

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use('/api/reservations', reservations);
 
 const PORT = process.env.PORT || 5003;
 
-const server = app.listen(PORT, console.log('Server running in', process.env.NODE_ENV, ' mode on port ', PORT))
+const server = app.listen(PORT, console.log('Server running in', process.env.NODE_ENV, "on" + process.env.HOST + ":" + PORT))
 
 process.on("unhandledRejection", (err, promise) => {
     console.log(`Error: ${err.message}`);
